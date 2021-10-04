94f2f8c65ed4782e6e75ba591bc11349127131b1
Hrishikesh Borkar <hrishirulz11@gmail.cm>
2021-10-03 20:51:02 -0600

    Added definitions

---
+++ definitions.py
@@ -0,0 +1,15 @@
+import yaml
+import os
+
+root = os.path.dirname(os.path.abspath(__file__))
+with open(root+'/config/config.yaml') as f:
+    cfg = yaml.safe_load(f)
+BLOCKSIZE = cfg['blocksize']
+
+print("CONFIG")
+print("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n")
+print(yaml.dump(cfg))
+print("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
+
+with open(root+'/config/config.yaml', 'w') as f:
+    data = yaml.dump(cfg, f)
